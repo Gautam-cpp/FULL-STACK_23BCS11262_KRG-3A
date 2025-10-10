b92b130c7173ea279534f3de2baba247476d3e6d
Gautam-cpp <gautamprajapat2509@gmail.com>
2025-10-10 20:11:20 +0530

    change

--- fs-mst/src/App.tsx
+++ fs-mst/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./Home"
-import Shopping from "./Shopping"
-import Todo from "./Todo"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Shopping from "./Shopping";
+import Todo from "./Todo";
 
 function App() {
   return (
-    <Router basename="/FULL-STACK_23BCS11262_KRG-3A">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shopping />} />
         <Route path="/todo" element={<Todo />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
